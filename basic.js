abe04b31520871f553b3db8530aa61a74cf2f98d
Naveen Kumar Thouday <thouday.naveen96@gmail.com>
2022-02-22 18:43:44 +0530

    variables discussion

---
+++ basic.js
@@ -0,0 +1,34 @@
+/*console.log("welcome to javascript");
+console.log(100+200);
+
+console.log("100"+100);
+console.log(+"100"+100);
+console.log(0.1+0.2);
+var sub_one= `reactjs`;
+var sub_two=`nodejs`;
+var sub_three=`mongoDB`;
+var mern=`MERN Stack: ${sub_one}<=>${sub_two}<=>${sub_three}`;
+console.log(mern);
+var flag= true;
+console.log(flag);
+var hexadecimalnumber= 0xABC123;
+console.log(hexadecimalnumber);
+var one_to = `naveen`;
+var one_two=`thouday`;
+var middle=`kumar`;
+var fullname= `${one_to} ${middle} ${one_two}`;
+console.log(fullname);
+
+
+var my_num = Symbol("reactjs");
+console.log(my_num);
+console.log(10==`10`); */
+
+
+//undefined and null
+console.log(undefined==undefined);
+console.log(undefined===undefined);
+console.log(null==null);
+console.log(null===null);
+console.log(undefined==null);
+console.log(undefined===null);
---
+++ sample.js
@@ -0,0 +1,9 @@
+const arr= [2,4,3,8,6];
+
+function double(x)
+{
+    return x*2;
+}
+const output=arr.map(double);
+console.log(arr);
+console.log(output);
